09a3d09f85e229bb636c1bf0e8667ee3c50839de
Eduardo Feitosa <eduardofeitosadg@gmail.com>
2019-06-16 21:26:45 -0300

    Calculator component

--- src/components/calculator/index.jsx
+++ src/components/calculator/index.jsx
@@ -3,33 +3,156 @@ import React, { Component, Fragment } from 'react';
 class Calculator extends Component {
 
   state = {
-    showForm: false
+    showForm: false,
+    firstValue: null,
+    secondValue: null,
+    result: null
+  }
+
+  style = {
+    input: {
+      width: '123px',
+      height: '25px',
+      padding: '5px',
+      border: '1px solid #ccc',
+      borderRadius: '3px',
+      marginTop: '5px'
+    },
+    button: {
+      width: '30px',
+      height: '30px',
+      float: 'left',
+      margin: '5px 5px 0px 0px'
+    },
+    clear: {
+      width: '135px',
+      height: '30px',
+      float: 'left',
+      margin: '5px 5px 0px 0px'
+    }
   }
 
   chargingForm = () => {
     this.setState({ showForm: true });
   }
 
+  handleFirstValue = event => {
+    this.setState({ firstValue: event.target.value });
+  }
+
+  handleSecondValue = event => {
+    this.setState({ secondValue: event.target.value });
+  }
+
+  clear = () => {
+    this.setState({
+      firstValue: null,
+      secondValue: null,
+      result: null
+    });
+    this.inputFirstValue.value = '';
+    this.inputSecondValue.value = '';
+  }
+
+  add = () => {
+    (this.state.firstValue && this.state.secondValue) &&
+      this.setState({
+        result:
+          parseFloat(this.state.firstValue) + 
+          parseFloat(this.state.secondValue)
+      });
+  }
+
+  subtract = () => {
+    (this.state.firstValue && this.state.secondValue) &&
+      this.setState({
+        result:
+          parseFloat(this.state.firstValue) - 
+          parseFloat(this.state.secondValue)
+      });
+  }
+
+  multiply = () => {
+    (this.state.firstValue && this.state.secondValue) &&
+      this.setState({
+        result:
+          parseFloat(this.state.firstValue) * 
+          parseFloat(this.state.secondValue)
+      });
+  }
+
+  share = () => {
+    (this.state.firstValue && this.state.secondValue) &&
+      this.setState({
+        result:
+          parseFloat(this.state.firstValue) / 
+          parseFloat(this.state.secondValue)
+      });
+  }
+
+  componentDidMount() {
+    this.chargingForm();
+  }
+
   render() {
 
-    const { showForm } = this.state;
+    const {
+      showForm,
+      result
+    } = this.state;
 
     return (
       <Fragment>
         {
           showForm ?
             <Fragment>
-              <p>Preencha todos os dados</p>
+              <p>Informe os valores para calcular</p>
               <p>
-                <input id="firstNumber" type="form" placeholder="Informe o primeiro valor" />
+                <input
+                  style={this.style.input}
+                  id="firstNumber"
+                  type="number"
+                  placeholder="Primeiro valor"
+                  onKeyUp={this.handleFirstValue}
+                  ref={el => this.inputFirstValue = el}
+                />
                 <br />
-                <input id="secondNumber" type="form" placeholder="Informe o segundo valor" />
+                <input
+                  style={this.style.input}
+                  id="secondNumber"
+                  type="number"
+                  placeholder="Segundo valor"
+                  onKeyUp={this.handleSecondValue}
+                  ref={el => this.inputSecondValue = el}
+                />
                 <br />
-                <button id="add">Somar</button> 
-                <button id="subtract">Subtrair</button> 
-                <button id="multiply">Multiplicar</button> 
-                <button id="share">Dividir</button> 
+                <button
+                  style={this.style.button}
+                  onClick={this.add}
+                  id="add">+</button>
+                <button
+                  style={this.style.button}
+                  onClick={this.subtract}
+                  id="subtract">-</button>
+                <button
+                  style={this.style.button}
+                  onClick={this.multiply}
+                  id="multiply">x</button>
+                <button
+                  style={this.style.button}
+                  onClick={this.share}
+                  id="share">/</button>
+                <br /><br />
+                <button
+                  style={this.style.clear}
+                  onClick={this.clear}
+                  id="clear">Limpar</button>
               </p>
+              <br />
+              {
+                result &&
+                <p>Resultado: {result}</p>
+              }
             </Fragment>
             :
             <p>Carregando calculadora...</p>
